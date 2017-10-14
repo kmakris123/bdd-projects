2b7560020e01b5e93a899cd4c67c411b618402ea
Kostas Makris <kostasmakris@Kostass-MacBook-Air.local>
2017-10-14 16:44:15 +0100

    add project2

--- src/main/java/myproject/myproject/WebDriverInit.java
+++ src/main/java/myproject/myproject/WebDriverInit.java
@@ -1,9 +1,10 @@
 package myproject.myproject;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class WebDriverInit {
 
@@ -14,10 +15,15 @@ public class WebDriverInit {
 		if (browserName.equals("firefox")) {
 			driver = new FirefoxDriver();
 		}
+		else if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "src/resources/bin/chromedriver/chromedriver");
+			driver = new ChromeDriver();
+		}
 		else {
 			throw new Exception("please provide a valid browser name");
 		}
-		driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 		return driver;
 		
 	}
--- src/main/java/myproject/myproject/pages/HomePage.java
+++ src/main/java/myproject/myproject/pages/HomePage.java
@@ -1,16 +1,14 @@
 package myproject.myproject.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
-
-import java.util.List;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 
 	public WebDriver driver;
+
 	
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
@@ -18,33 +16,21 @@ public class HomePage {
 	
 	public boolean isPageLoaded () {
 
-		return driver.findElement(By.cssSelector("input[value='Search']")).isDisplayed();
+		driver.get("http://www.zoopla.co.uk/");
+		new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(driver.findElement(By.id("logo"))));
+		return driver.findElement(By.id("logo")).isDisplayed();
 	}
 
 
-	public void  selectCategory (String category, String item) {
-		Select categoriesElement = new Select(driver.findElement(By.id("gh-cat")));
-		categoriesElement.selectByVisibleText(category);
-	}
+public Boolean getPropertyRentSection () {
+    driver.findElement(By.cssSelector("a[href='/to-rent/']")).click();
+	return driver.findElement(By.cssSelector("p[class='h-subheader']")).getText().equals("Search for houses and flats to rent across the UK");
 
-	public void searchForItem(String item) {
-		driver.findElement(By.cssSelector("input[placeholder='Search...']")).sendKeys(item);
-		driver.findElement(By.cssSelector("input[value='Search']")).click();
-
-	}
-
-	public void sortBySearch(String sorting) {
-		WebElement dropDownMenu = driver.findElement(By.id("SortMenu"));
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("arguments[0].setAttribute('style', 'visibility: visible;');", dropDownMenu);
-		List<WebElement> options = dropDownMenu.findElements(By.tagName("li"));
-
-		for (WebElement option : options) {
-			if(sorting.equals(option.getText()))
-				option.click();
-			break;
-		}
-
-	}
-	
 }
+
+
+
+
+}
+	
+
